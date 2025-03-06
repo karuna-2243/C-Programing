d2850bf49e3955d7b22e8e23f101548fd2d74cee
Karuna Shinde <karunashinde22042003@gmail.com>
2025-03-06 13:32:49 +0530

    Subtaction of two Numbers

---
+++ subraction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+    int num1,num2,result;
+
+    printf("enter the first number\n");
+    scanf("%d",&num1);
+
+    printf("enter the second number\n");
+    scanf("%d",&num2);
+
+    result=num1 - num2;
+
+    printf("the subtraction is %d",result);
+
+}
