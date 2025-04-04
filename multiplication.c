dbe352dac3cb99fe5029e7b97c68c63a096d65eb
Atharva-Dighe <atharvawork.3@gmail.com>
2025-04-04 09:40:00 +0530

    Add multiplication program

---
+++ multiplication.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    int num1, num2, result;
+
+    printf("Enter the first number:\n");
+    scanf("%d", &num1);
+
+    printf("Enter the second number:\n");
+    scanf("%d", &num2);
+
+    result = num1 * num2;
+    
+
+    printf("The multiplication is: %d\n", result);
+}
